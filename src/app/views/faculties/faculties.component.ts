0dbdfccb91ec5ae83c626f3da462df3a88b8acb1
umer <md.farooq.ansari8959149@gmail.com>
2024-04-16 00:50:27 +0500

    faulties grid has designed

--- src/app/views/faculties/faculties.component.ts
+++ src/app/views/faculties/faculties.component.ts
@@ -9,5 +9,44 @@ import { SharedModule } from 'src/app/modules/shared/shared.module';
   styleUrl: './faculties.component.scss'
 })
 export class FacultiesComponent {
-
+  tableColumns : any = [
+    "#",
+    "name",
+    "email",
+    "department",
+    "type",
+    "action",
+  ];
+  data : any = [
+    {
+      name : 'mack',
+      email : 'mack@example.com',
+      department : 'Sceince',
+      type : 'professor',
+    },
+    {
+      name : 'Azhar',
+      email : 'azhar@example.com',
+      department : 'Medical',
+      type : 'assistant professor',
+    },
+    {
+      name : 'Annie',
+      email : 'annie@example.com',
+      department : 'Sceince',
+      type : 'assistant professor',
+    },
+    {
+      name : 'Faiq',
+      email : 'faiq@example.com',
+      department : 'Medical',
+      type : 'assistant professor',
+    },
+    {
+      name : 'shawn',
+      email : 'shawn@example.com',
+      department : 'Sceince',
+      type : 'professor',
+    },
+  ]
 }
