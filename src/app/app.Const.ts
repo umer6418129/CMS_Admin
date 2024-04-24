1c2ae3c250d87af41dff1a6e030b95e11a0851c9
Muhammad Umer Farooq <121379696+umer6418129@users.noreply.github.com>
2024-04-24 05:05:45 +0000

    created app contant

---
+++ src/app/app.Const.ts
@@ -0,0 +1,30 @@
+export class AppConst {
+    static MAIN_ROUTES = {
+        addmissions: 'addmissions',
+        courses : 'courses',
+        subjects: 'subjects',
+        faculties : 'faculties',
+        dashboard: 'dashboard',
+        FAQ: 'faqs',
+        CONTACT: 'contact-us',
+        SIGNUP: 'signup',
+        LOGIN: 'login',
+        PORTAL: 'portal',
+        CART: 'cart',
+        REGISTER: 'register',
+        ADDMISSION: 'addmission',
+        FACILITIES: 'facilities',
+        DEPARTMENT: 'department',
+        FACULTIES: 'faculties',
+    }
+    static URL_REQUEST = {
+        // ContactUs: 'ContactUs',
+        // feturedCourses: 'Courses/featured',
+        // courses: 'Courses',
+        // feedBack: 'FeedBack',
+        // department: 'Department',
+        // Faculties: 'Faculties',
+        // Student: 'Student',
+
+    }
+}
